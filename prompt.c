a9e1f178e06c58499b3af5748f82f1f140dadbd3
AthmanDodoe <jilloathman943@gmail.com>
2022-06-13 14:37:11 +0300

    prompt

---
+++ prompt.c
@@ -0,0 +1,11 @@
+#include "simpleshell.h"
+/**
+ * prompt_shell - function to print the prompt
+ * @void: wherever
+ * Return: none
+ */
+void prompt_shell(void)
+{
+	write(STDOUT, "\033[1;31mMi_shell/user@root$ ", 32);
+	write(STDOUT, "\033[0m", 4);
+}
