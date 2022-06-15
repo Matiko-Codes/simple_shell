7ced9f28a3821160dac906b263b9d2ef36f61a82
Matiko <mmatiko243@gmail.com>
2022-06-15 03:51:39 +0000

    strConcat.c

--- strConcat.c
+++ strConcat.c
@@ -14,7 +14,7 @@ s1 = "";
 if (s2 == NULL)
 s2 = "";
 size = _strlen(s1) + _strlen(s2) + 2;
-arr = (char *)malloc(size * sizeof(char));
+arr = (char *)malloc(size *sizeof(char));
 if (arr == 0)
 return (NULL);
 for (i = 0; s1[i] != '\0'; i++)
